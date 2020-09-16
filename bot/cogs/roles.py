6e1bddb6986eeefaf43cfbad785586d3367e8b71
Nathan Estrada <naestrada19@gmail.com>
2020-09-16 09:46:26 -0700

    In progress verify code

--- bot/cogs/roles.py
+++ bot/cogs/roles.py
@@ -8,12 +8,21 @@ from bot.reference import *
 class Roles(commands.Cog):
 
     def __init__(self, bot):
-        self.bot = bot;
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         role = discord.utils.get(member.guild.roles, id=755660727305633813)
         await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        role = discord.utils.get(user.guild.roles, id=755660727305633813)
+        channel = reaction.message.channel
+        print(channel.id)
+        if channel.id == 755830287182725282:
+            if reaction.emoji == "👍":
+                await user.add_roles(role)
 
 def setup(bot):
     bot.add_cog(Roles(bot))
